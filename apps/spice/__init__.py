a7227bd7ce67adb6689e61e81dcbcf6ad135b160
sabrina-440 <sabrinaxjp@gmail.com>
2026-02-22 13:15:20 -0800

    inital

---
+++ apps/spice/__init__.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.helpers.typing import ConfigType
+
+import appdaemon.plugins.hass.hassapi as hass
+import spiceapi
+from spiceapi import card_insert
+import argparse
+from configparser import RawConfigParser
+
+
+#### helper functions ####
+
+def connect_spice(host, port, password):
+    return spiceapi.Connection(host, port, password)
+
+def p1Card():
+
+    #  get config from default.ini
+    parser = argparse.ArgumentParser(description='spicehue')
+    parser.add_argument("--config", type=str, default='default.ini')
+    args = parser.parse_args()
+    
+    config = RawConfigParser()
+    config.optionxform = lambda option: option
+
+    config.read(args.config)
+    spice_host = config.get('spice', 'Host')
+    spice_port = config.getint('spice', 'Port')
+    spice_pass = config.get('spice', 'Password', fallback='')
+    player1    = config.get('cards', 'PersonA')
+
+    # connect to spice
+
+    print(f'Connect to SpiceAPI... host: {spice_host}, port: {spice_port}')
+    try:
+        con = connect_spice(spice_host, spice_port, spice_pass)
+    except Exception:
+        print("Sconnection failed")
+    
+    card_insert(con, 1, player1)
+
+def p2Card():
+
+    #  get config from default.ini
+    parser = argparse.ArgumentParser(description='spicehue')
+    parser.add_argument("--config", type=str, default='default.ini')
+    args = parser.parse_args()
+    
+    config = RawConfigParser()
+    config.optionxform = lambda option: option
+
+    config.read(args.config)
+    spice_host = config.get('spice', 'Host')
+    spice_port = config.getint('spice', 'Port')
+    spice_pass = config.get('spice', 'Password', fallback='')
+    player2    = config.get('cards', 'PersonB')
+
+    # connect to spice
+
+    print(f'Connect to SpiceAPI... host: {spice_host}, port: {spice_port}')
+    try:
+        con = connect_spice(spice_host, spice_port, spice_pass)
+    except Exception:
+        print("Sconnection failed")
+    
+    card_insert(con, 2, player2)
+
+###########
+
+DOMAIN = "spice"
+
+async def async_setup(hass, config):
+    async def handle_p1_cardin(call): 
+        p1Card()
+    async def handle_p2_cardin(call): 
+        p2Card()
+    async def handle_both_cardin(call): 
+        p1Card()
+        p2Card()
+
+    hass.services.async_register(
+        DOMAIN,
+        "P1CardIn",
+        handle_p1_cardin
+    )
+
+    hass.services.async_register(
+        DOMAIN,
+        "P2CardIn",
+        handle_p2_cardin
+    )
+
+    hass.services.async_register(
+        DOMAIN,
+        "MultiplayerCardIn",
+        handle_both_cardin
+    )
+
+    return True
+
+
+
+
+
